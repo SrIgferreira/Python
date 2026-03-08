d93ffb0b0876191a9c190325aaf55eed4fb46f5d
Ian Ferreira <sr.igferreira@gmail.com>
2026-03-08 17:53:12 -0300

    aula06B.py

---
+++ aula06A.py
@@ -0,0 +1,4 @@
+n1 = int(input("Digite a primeira nota: "))
+n2 = int(input("Digite a segunda nota: "))
+s = n1 + n2
+print ("A soma entre {} e {} é igual a {}".format(n1, n2, s))
---
+++ aula06B.py
@@ -0,0 +1,13 @@
+n = input("Digite um número: ")
+print(n.isnumeric())
+#.isalpha() - Verifica se é alfanumérico
+#.isupper() - Verifica se é maiúsculo
+#.islower() - Verifica se é minúsculo
+#isalnum() - Verifica se é alfanumérico (letras e números)
+#.isdecimal() - Verifica se é decimal (números inteiros)
+#isdigit() - Verifica se é dígito (números inteiros)
+#isnumeric() - Verifica se é numérico (números inteiros e decimais)
+#isprintable() - Verifica se é imprimível (caracteres que podem ser impressos)
+#isspace() - Verifica se é espaço em branco (espaços, tabulações, etc)
+#istitle() - Verifica se é título (primeira letra maiúscula e as outras minúsculas)
+#isidentifier() - Verifica se é um identificador válido (pode ser usado como nome de variável, função, etc)
