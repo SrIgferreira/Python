63309480fcefc6bc870302d818a3fcaa56ccb8e7
Ian Ferreira <sr.igferreira@gmail.com>
2026-03-08 16:59:37 -0300

    desafio01.py

--- desafio01.py
+++ desafio01.py
@@ -1,2 +1,2 @@
 name=input('Qual é o seu nome? ')
-print("Olá", name,". Prazer em te conhecer, sem bem-vindo ao mundo da programação. Parabéns pelo seu primeiro código.")
+print("Olá, {}. Prazer em te conhecer, sem bem-vindo ao mundo da programação. Parabéns pelo seu primeiro código.".format(name))
