26d18662f7f53a9836274cadc0315665dac70cda
Ian Ferreira <sr.igferreira@gmail.com>
2026-03-23 16:23:26 -0300

    Delete desafio08.py

--- desafio08.py
+++
@@ -1,5 +0,0 @@
-n=int(input("Digite um número em metros: "))
-cm = n * 100
-mm = n * 1000
-print("O número digitado em centímetros é: {}cm".format(cm), ";", end=" ")
-print("O número digitado em milímetros é: {}mm".format(mm))
