c1613e9b2437475302b52bf51a97ddeb4c73a4a0
Ian Ferreira <sr.igferreira@gmail.com>
2026-03-23 16:29:01 -0300

    Delete desafio11.py

--- desafio11.py
+++
@@ -1,6 +0,0 @@
-l=float(input("Digite a largura da parede: "))
-a=float(input("Digite a altura da parede: "))
-area = l * a
-tinta = area / 2
-print("A área da parede é: {}m²".format(area), ";", end=" ")
-print("A quantidade de tinta necessária para pintar a parede é: {}L".format(tinta))
