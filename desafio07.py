bf704bacaaa048e7c701a2c5ce85c2ba6d435073
Ian Ferreira <sr.igferreira@gmail.com>
2026-03-23 16:21:14 -0300

    Delete desafio07.py

--- desafio07.py
+++
@@ -1,5 +0,0 @@
-n = str(input("Digite seu nome completo: "))
-n1 = float(input("Digite a primeira nota: "))
-n2 = float(input("Digite a segunda nota: "))
-m = (n1 + n2) / 2
-print("O aluno {} tem uma média de: {:.1f}".format(n, m))
